3458025a666e33a5ee3ec7a4be7960653c9f432c
breyr <breyr2021@gmail.com>
2023-09-11 23:25:13 -0400

    Test for D1 connection

--- workers.js
+++ workers.js
@@ -2,23 +2,11 @@
  * GET /workers
  */
 
-const workers = document.getElementById('workersContainer');
-
 export async function fetchAndPopulateList(context) {
     // Create a prepared statement with our query
     // ps stands for "prepared statement"
     const ps = context.env.DB.prepare('SELECT * from students');
-    const { results } = await ps.all().json();
-
-    // Clear any existing list items
-    workers.innerHTML = '';
-
-    // Loop through the data and create list items
-    results.forEach(item => {
-        const listItem = document.createElement('li');
-        listItem.textContent = item.name; // Assuming 'name' is a property in your data
-        workers.appendChild(listItem);
-    });
+    const { results } = await ps.all();
 
     return Response.json(results);
 }
